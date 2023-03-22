0d55e88c0cab033ff75bd8d66fd4075e645e66a0
Nina He <65968753+hhe04@users.noreply.github.com>
2023-03-22 11:58:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { IConfig } from "./interface";
 
 export const config: IConfig = {
   api: process.env.API || "https://api.openai.com",
-  openai_api_key: process.env.OPENAI_API_KEY || "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",
+  openai_api_key: process.env.OPENAI_API_KEY || "123456",
   model: process.env.MODEL || "gpt-3.5-turbo",
   chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
   chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "",
